acba3d7037c0217913f6c4e4068dc7f21e0bf23d
bumwoo <gld55602@gmail.com>
2023-02-05 13:05:04 +0900

    style: 불필요한 import, 주석 제거

--- src/main/java/com/health/healthlog/domain/Body.java
+++ src/main/java/com/health/healthlog/domain/Body.java
@@ -4,16 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.time.LocalDateTime;
 import lombok.Getter;
 
 @Getter
 @Entity
 public class Body extends BaseTimeEntity {
-    // - 체중
-    // - 골격근
-    // - 체지방
-    // - 년, 월
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
