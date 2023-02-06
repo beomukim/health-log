62bf866779f3bb5f93fc3ebe4991f2e98342cac7
bumwoo <gld55602@gmail.com>
2023-02-06 15:52:59 +0900

    feat: dto, searchType 설계

---
+++ src/main/java/com/health/healthlog/domain/type/SearchType.java
@@ -0,0 +1,5 @@
+package com.health.healthlog.domain.type;
+
+public enum SearchType {
+    CONTENT, TRAINING, DATE
+}
---
+++ src/main/java/com/health/healthlog/dto/TrainingDto.java
@@ -0,0 +1,4 @@
+package com.health.healthlog.dto;
+
+public record TrainingDto() {
+}
