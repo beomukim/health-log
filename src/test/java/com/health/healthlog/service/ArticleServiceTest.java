f99b9b903ab9363b4db998a0bab02546073e678b
bumwoo <gld55602@gmail.com>
2023-02-07 21:43:04 +0900

    test: 게시글 생성, 수정, 삭제 테스트 코드 작성

--- src/test/java/com/health/healthlog/service/ArticleServiceTest.java
+++ src/test/java/com/health/healthlog/service/ArticleServiceTest.java
@@ -3,12 +3,15 @@ package com.health.healthlog.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.willDoNothing;
 import static org.mockito.Mockito.mock;
 
 import com.health.healthlog.domain.Article;
 import com.health.healthlog.domain.type.SearchType;
 import com.health.healthlog.dto.ArticleDto;
 import com.health.healthlog.repository.ArticleRepository;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
@@ -37,7 +40,7 @@ public class ArticleServiceTest {
         List<ArticleDto> articles = sut.searchArticles(SearchType.CONTENT, "search keyword");
 
         // Then
-        assertThat(articles).isNotNull().hasSize(2);
+        assertThat(articles).isNotNull().hasSize(3);
     }
 
     @DisplayName("게시글을 조회하면, 게시글을 반환한다.")
@@ -46,11 +49,52 @@ public class ArticleServiceTest {
         // Given
         Article result = new Article("id 15");
         given(articleRepository.findById(any())).willReturn(Optional.of(result));
+
         // When
         ArticleDto article = sut.searchArticle(15L);
 
         // Then
         ArticleDto articleDto = ArticleDto.from(result);
         assertThat(article).isNotNull().isEqualTo(articleDto);
+    }
+
+    @DisplayName("게시글 정보를 입력하면, 게시글을 생성한다")
+    @Test
+    void givenArticleInfo_whenSavingArticle_thenSavesArticle() {
+        // Given
+        given(articleRepository.save(any(Article.class))).willReturn(null);
+
+        // When
+        sut.saveArticle(ArticleDto.of(1L, "content", LocalDateTime.now()));
+
+        // Then
+        then(articleRepository).should().save(any(Article.class));
+    }
+
+    @DisplayName("게시글의 ID와 수정 정보를 입력하면, 게시글을 수정한다")
+    @Test
+    void givenArticleIdAndModifiedInfo_whenUpdatingArticle_thenUpdatesArticle() {
+        // Given
+        Article article = new Article("article");
+        given(articleRepository.getReferenceById(15L)).willReturn(article);
+
+        // When
+        sut.updateArticle(15L, ArticleDto.of("new content"));
+
+        // Then
+        then(articleRepository).should().getReferenceById(15L);
+    }
+
+    @DisplayName("게시글의 ID를 입력하면, 게시글을 삭제한다")
+    @Test
+    void givenArticleId_whenDeletingArticle_thenDeletesArticle() {
+        // Given
+        willDoNothing().given(articleRepository).deleteById(any());
+
+        // When
+        sut.deleteArticle(1L);
+
+        // Then
+        then(articleRepository).should().deleteById(1L);
     }
 }
