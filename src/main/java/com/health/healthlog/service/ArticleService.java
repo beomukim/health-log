93a3470342bb1b53ad8ee39408a55f77dacaa978
bumwoo <gld55602@gmail.com>
2023-02-07 21:42:41 +0900

    feat: 게시글 update, delete 구현

--- src/main/java/com/health/healthlog/service/ArticleService.java
+++ src/main/java/com/health/healthlog/service/ArticleService.java
@@ -1,15 +1,17 @@
 package com.health.healthlog.service;
 
+import com.health.healthlog.domain.Article;
 import com.health.healthlog.domain.type.SearchType;
 import com.health.healthlog.dto.ArticleDto;
-import com.health.healthlog.dto.ArticleUpdateDto;
 import com.health.healthlog.repository.ArticleRepository;
 import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -30,11 +32,21 @@ public class ArticleService {
     }
 
     public void saveArticle(ArticleDto dto) {
+        articleRepository.save(dto.toEntity());
     }
 
-    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    public void updateArticle(long articleId, ArticleDto dto) {
+        try {
+            Article article = articleRepository.getReferenceById(articleId);
+            if (dto.content() != null) {
+                article.setContent(dto.content());
+            }
+        } catch (EntityNotFoundException e) {
+            log.warn("게시글 업데이트 실패. 게시글을 수정하는데 필요한 정보를 찾을 수 없습니다 - {}", e.getLocalizedMessage());
+        }
     }
 
     public void deleteArticle(long articleId) {
+        articleRepository.deleteById(articleId);
     }
 }
