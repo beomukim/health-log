a020b280c6eb4fb84a4b6c29702ee5ded5347f2b
bumwoo <gld55602@gmail.com>
2023-02-09 22:43:20 +0900

    feat: searchType description 추가

--- src/main/java/com/health/healthlog/domain/type/SearchType.java
+++ src/main/java/com/health/healthlog/domain/type/SearchType.java
@@ -1,5 +1,14 @@
 package com.health.healthlog.domain.type;
 
+import lombok.Getter;
+
 public enum SearchType {
-    CONTENT, TRAINING, DATE
+    CONTENT("내용"), TRAINING("트레이닝"), DATE("날짜");
+
+    @Getter
+    private final String description;
+
+    SearchType(String description) {
+        this.description = description;
+    }
 }
