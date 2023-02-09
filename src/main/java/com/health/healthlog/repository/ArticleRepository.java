5cd7d541326bd957596daeb0e9f71e96afc252f9
bumwoo <gld55602@gmail.com>
2023-02-09 20:29:01 +0900

    feat: spring data rest 설정

--- src/main/java/com/health/healthlog/repository/ArticleRepository.java
+++ src/main/java/com/health/healthlog/repository/ArticleRepository.java
@@ -1,7 +1,11 @@
 package com.health.healthlog.repository;
 
 import com.health.healthlog.domain.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
--- src/main/java/com/health/healthlog/repository/TrainingRepository.java
+++ src/main/java/com/health/healthlog/repository/TrainingRepository.java
@@ -2,6 +2,8 @@ package com.health.healthlog.repository;
 
 import com.health.healthlog.domain.Training;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface TrainingRepository extends JpaRepository<Training, Long> {
 }
