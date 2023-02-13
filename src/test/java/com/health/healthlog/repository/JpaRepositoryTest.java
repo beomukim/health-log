d4aa9a97cfe0e1b9c7f12a7f89a3362638b91da6
bumwoo <gld55602@gmail.com>
2023-02-13 16:51:37 +0900

    test(JpaRepositoryTest): UserAccount 변경사항 적용

--- src/test/java/com/health/healthlog/repository/JpaRepositoryTest.java
+++ src/test/java/com/health/healthlog/repository/JpaRepositoryTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.health.healthlog.domain.Article;
 import com.health.healthlog.domain.Training;
+import com.health.healthlog.domain.UserAccount;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
@@ -17,12 +19,15 @@ public class JpaRepositoryTest {
 
     private ArticleRepository articleRepository;
     private TrainingRepository trainingRepository;
+    private UserAccountRepository userAccountRepository;
 
     public JpaRepositoryTest(
             @Autowired ArticleRepository articleRepository,
-            @Autowired TrainingRepository trainingRepository) {
+            @Autowired TrainingRepository trainingRepository,
+            @Autowired UserAccountRepository userAccountRepository) {
         this.articleRepository = articleRepository;
         this.trainingRepository = trainingRepository;
+        this.userAccountRepository = userAccountRepository;
     }
 
     @DisplayName("select 테스트")
@@ -50,7 +55,7 @@ public class JpaRepositoryTest {
     void givenTestData_whenInserting_thenWorksFine() {
         // Given
         long previousCount = articleRepository.count();
-        Article article = new Article("new content");
+        Article article = new Article(UserAccount.of("beomu", "pw", null, null, null), "new content");
 
         // When
         articleRepository.save(article);
@@ -63,7 +68,7 @@ public class JpaRepositoryTest {
     @Test
     void givenTestData_whenUpdating_thenWorksFine() {
         // Given
-        Article article = articleRepository.findById(1L).orElseThrow();
+        Article article = articleRepository.findById(1L).orElseThrow(EntityNotFoundException::new);
         String updatedContent = "this is health log.";
         article.setContent(updatedContent);
 
@@ -78,7 +83,7 @@ public class JpaRepositoryTest {
     @Test
     void givenTestData_whenDeleting_thenWorksFine() {
         // Given
-        Article article = articleRepository.findById(15L).orElseThrow();
+        Article article = articleRepository.findById(14L).orElseThrow();
 
         long previousArticleCount = articleRepository.count();
         long previousTrainingCommentCount = trainingRepository.count();
