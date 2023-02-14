070d91d5cc6633c23849578c58ffc1062ed07180
bumwoo <gld55602@gmail.com>
2023-02-15 02:02:00 +0900

    feat: data.sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user_account (user_id, user_password, nickname, email, memo, created_at, modified_at) values
+    ('beomu', '{noop}asdf1234', 'beomu', 'beomu@email.com', 'I am beoum.', now(), now());
+
+insert into user_account (user_id, user_password, nickname, email, memo, created_at, modified_at) values
+    ('yang', '{noop}asdf1234', 'yang', 'yang@email.com', 'I am yang.', now(), now());
