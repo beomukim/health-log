71d5bc67c924d682a8012fbecc83804c79b2cb06
bumwoo <gld55602@gmail.com>
2023-02-19 15:59:04 +0900

    test: 카카오 인증 테스트코드 작성

--- src/test/java/com/health/healthlog/config/TestSecurityConfig.java
+++ src/test/java/com/health/healthlog/config/TestSecurityConfig.java
@@ -1,30 +1,37 @@
 package com.health.healthlog.config;
-import com.health.healthlog.domain.UserAccount;
-import com.health.healthlog.repository.UserAccountRepository;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.event.annotation.BeforeTestMethod;
-
-import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+
+import com.health.healthlog.dto.UserAccountDto;
+import com.health.healthlog.service.UserAccountService;
+import java.util.Optional;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
 
 @Import(SecurityConfig.class)
 public class TestSecurityConfig {
 
     @MockBean
-    private UserAccountRepository userAccountRepository;
+    private UserAccountService userAccountService;
 
     @BeforeTestMethod
     public void securitySetUp() {
-        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+        given(userAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(), anyString(), anyString(), anyString(), anyString()))
+                .willReturn(createUserAccountDto());
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
                 "beomuTest",
                 "pw",
                 "beomu-test@email.com",
                 "beomu-test",
                 "test memo"
-        )));
+        );
     }
 
 }
